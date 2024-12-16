3846eda3eab8c1b12ece81d6cd046e6fb1e60f98
Shiv Shankar <shankarshiv470@gamil.com>
2024-12-16 10:46:48 +0530

    added description and used swagger ui tool for Bank API

--- banking-app/src/main/java/com/example/banking_app/BankingAppApplication.java
+++ banking-app/src/main/java/com/example/banking_app/BankingAppApplication.java
@@ -3,7 +3,33 @@ package com.example.banking_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot banking Application",
+				description = "Backend REST API for banking appkication",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Shiv Shankar",
+						email = "shankarshiv470@gmail.com",
+						url = "https://github.com/Shivshankar108/spring-banking-app"
+				),
+				license = @License(
+						name = "Shiv Shankar",
+						url = "https://github.com/Shivshankar108/spring-banking-app"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot Banking Application Description",
+				url = "https://github.com/Shivshankar108/spring-banking-app"
+		)
+)
 public class BankingAppApplication {
 
 	public static void main(String[] args) {
--- banking-app/src/main/java/com/example/banking_app/controller/UserController.java
+++ banking-app/src/main/java/com/example/banking_app/controller/UserController.java
@@ -12,43 +12,102 @@ import com.example.banking_app.dto.TransferRequest;
 import com.example.banking_app.dto.UserRequest;
 import com.example.banking_app.service.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/user")
+@Tag(name = "User Account Management APIs")
 public class UserController {
 
 	UserService userService;
 
+
+//  Constructor Injection dependency
 	public UserController(UserService userService) {
 		this.userService = userService;
 	}
 	
+	
+	@Operation(
+			summary = "Create New User Account",
+			description = "Creating a new user and assigning an account number"
+	)
+	@ApiResponse(
+			responseCode = "201",
+			description = "Http Status 201 CREATED"
+	)
 	@PostMapping("/")
 	public BankResponse createAccount(@RequestBody UserRequest userRequest) {
 		return userService.createAccount(userRequest);
 	}
 	
 	
+	@Operation(
+			summary = "Balance Enquiry",
+			description = "Provide with balance info of the given account number"
+	)
+	@ApiResponse(
+			responseCode = "200",
+			description = "Http Status 200 SUCCESS"
+	)
 	@PostMapping("/balanceEnquiry")
 	public BankResponse balanceEnquiry(@RequestBody EnquiryRequest request) {
 		return userService.balanceEnquiry(request);
 	}
 	
+	
+	@Operation(
+			summary = "Name Enquiry",
+			description = "Provide with Account info of the given account number"
+	)
+	@ApiResponse(
+			responseCode = "200",
+			description = "Http Status 200 SUCCESS"
+	)
 	@PostMapping("/nameEnquiry")
 	public String nameEnquiry(@RequestBody EnquiryRequest request) {
 		return userService.nameEnquiry(request);
 	}
 	
+	
+	@Operation(
+			summary = "Amount Credit",
+			description = "credit the given amount in the given account number if exists"
+	)
+	@ApiResponse(
+			responseCode = "200",
+			description = "Http Status 200 SUCCESS"
+	)
 	@PostMapping("/credit")
 	public BankResponse creditAccount(@RequestBody CreditAndDebitRequest request) {
 		return userService.creditAccount(request);
 	}
 	
+	
+	@Operation(
+			summary = "Account Debite",
+			description = "Debit amount from the given account number if the account exists and the amount is sufficient in account"
+	)
+	@ApiResponse(
+			responseCode = "200",
+			description = "Http Status 200 SUCCESS"
+	)
 	@PostMapping("/debit")
 	public BankResponse debitAccount (@RequestBody CreditAndDebitRequest request) {
 		return userService.debitAccount(request);
 	}
+
 	
-	
+	@Operation(
+			summary = "Transfer of given amount from one account to another",
+			description = "Transfer of given amount from one acount to another if the both given account exists and the sender has a sufficient balance in their account"
+	)
+	@ApiResponse(
+			responseCode = "200",
+			description = "Http Status 200 SUCCESS"
+	)
 	@PostMapping("/transfer")
 	public BankResponse transfer(@RequestBody TransferRequest request) {
 		return userService.transferMoney(request);
